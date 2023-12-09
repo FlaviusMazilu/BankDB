f4587bdbf4dbb4636cef391f5c61934bc1e23e2b
FlaviusMazilu <94180209+FlaviusMazilu@users.noreply.github.com>
2023-12-09 19:45:16 +0200

    Add homepage data from db

--- my_bank/src/componets/TransactionItem.js
+++ my_bank/src/componets/TransactionItem.js
@@ -11,10 +11,14 @@ export const TransactionItem = ({sender, receiver, amount}) => {
       // navigate("/stats/" + songId);
     }}>
       <Grid item xs={12} style={{ padding: "10px" }}>
+      <div className="t-container">
+        <img src="./right-arrow.svg" alt="Your SVG Icon" className="right-arrow"></img>
         <div className="sender">{sender}</div>
         <div className="receiver">{receiver}</div>
         <div className="amount">{amount} </div>
+      </div>
       </Grid>
+      
     </CardActionArea>
   );
 };
