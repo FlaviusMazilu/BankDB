982d8469248cf4470b8633522d2c82efc8257737
Earthbert <daraban_timotei@tutanota.com>
2023-12-09 12:59:37 +0200

    Add stuff

---
+++ my_bank/src/componets/Main.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+const Test = () => {
+	return (
+		<Grid container spacing={2}>
+
+			<Grid item xs={12}>
+				<Paper variant='elevation'>
+					{/* Welcome section */}
+					<h1>Welcome to My Bank</h1>
+				</Paper>
+			</Grid>
+			<Grid item xs={12}>
+				<Paper>
+					{/* Current balance section */}
+					<h2>Current Balance: $1000</h2>
+				</Paper>
+			</Grid>
+			<Grid item xs={12}>
+				<Paper>
+					{/* Last three transactions section */}
+					<h2>Last Three Transactions:</h2>
+					<ul>
+						<li>Transaction 1</li>
+						<li>Transaction 2</li>
+						<li>Transaction 3</li>
+					</ul>
+				</Paper>
+			</Grid>
+			<Grid item xs={12}>
+				<Paper>
+					{/* News section */}
+					<h2>News:</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</Paper>
+			</Grid>
+			<Grid item xs={12}>
+				<Paper variant='outlined'>
+					{/* Buttons section */}
+					<button>Home</button>
+					<button>Transactions</button>
+					<button>Cashback</button>
+					<button>Eco Features</button>
+				</Paper>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default Test;
