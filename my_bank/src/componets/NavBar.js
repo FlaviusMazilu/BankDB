2997a19af7d3900236f337874e589c67c7112314
Valentin Bobaru <bobvalentin2003@gmail.com>
2023-12-09 16:35:59 +0200

    add navBar

--- my_bank/src/componets/NavBar.js
+++ my_bank/src/componets/NavBar.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+
+let curr_butt = 0;
 
 const NavBar = () => {
+	const [value, setValue] = React.useState(curr_butt);
 	return (
-		<Grid item xs={12}>
-			<Paper variant='outlined'>
-				{/* Navigation bar */}
-				<nav style={{ position: 'fixed', bottom: 0, backgroundColor: 'white', width: '100vw' }}>
-					<ul style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', listStyleType: 'none' }}>
-						<li><Button component={Link} to="/">Home</Button></li>
-						<li><Button component={Link} to="/transactions">Transactions</Button></li>
-						<li><Button component={Link} to="/cashback">Cashback</Button></li>
-						<li><Button component={Link} to="/eco">Go Eco</Button></li>
-					</ul>
-				</nav>
-			</Paper>
-		</Grid>
+		// <Grid item xs={12}>
+		// 	<Paper variant='outlined'>
+		// 		{/* Navigation bar */}
+		// 		<nav style={{ position: 'absolute',  left:'10vw', bottom: '5vw', backgroundColor: 'black', width: '80vw' }}>
+		// 			<ul style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', listStyleType: 'none' }}>
+		// 				<li><Button style={{display: 'flex'}} component={Link} to="/">Home</Button></li>
+		// 				<li><Button style={{display: 'flex'}} component={Link} to="/transactions">Transactions</Button></li>
+		// 				<li><Button style={{display: 'flex'}} component={Link} to="/cashback">Cashback</Button></li>
+		// 				<li><Button style={{display: 'flex'}} component={Link} to="/eco">Go Eco</Button></li>
+		// 			</ul>
+		// 		</nav>
+		// 	</Paper>
+		// </Grid>
+		
+
+		<BottomNavigation
+			style={{ display:'flex', justifyContent:'center', position: 'absolute', width:'100vw',  bottom: '5vw', backgroundColor: "gray"}}
+			showLabels
+        	value={value}
+        	onChange={(event, newValue) => {
+				console.log(newValue);
+			curr_butt = newValue;
+          	setValue(newValue);
+        }}>
+			<BottomNavigationAction style={{position:'relative', width: "30vw"}} label={"Home"} component={Link} to="/" />
+			<BottomNavigationAction style={{position:'relative', width: "30vw"}} label={"Transactions"} component={Link} to="/transactions" />
+			<BottomNavigationAction style={{position:'relative', width: "30vw"}} label={"Go Eco"} component={Link} to="/eco" />
+		</BottomNavigation>
 	);
 }
 
