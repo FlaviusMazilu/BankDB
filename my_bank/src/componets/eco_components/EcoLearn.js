c7835bf9f1f73428adc8329943fb860bca0474ef
Valentin Bobaru <bobvalentin2003@gmail.com>
2023-12-10 00:05:51 +0200

    add ecopages skel

--- my_bank/src/componets/eco_components/EcoLearn.js
+++ my_bank/src/componets/eco_components/EcoLearn.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Typography, Card, CardContent } from '@mui/material';
+import { Typography, Card, CardContent, Grid } from '@mui/material';
 import EcoNavBar from './EcoNavBar';
 import NavBar from '../NavBar';
+import {Link} from '@mui/material';
 
 const EcoLearn = () => {
 	return (
 		<div>
 			<EcoNavBar />
-			<Card>
-				<CardContent>
-					<Typography variant="h5" component="h2">
-						Useful Information about Ecology
-					</Typography>
-					<Typography variant="body2" component="p">
-						Insert your useful information here...
-					</Typography>
-				</CardContent>
-			</Card>
+			<p style={{textAlign:'justify', marginLeft:'5vw', marginRight:'5vw' }}>
+			Maintaining a healthy environment is crucial for the well-being of both humanity and the planet. The delicate balance of ecosystems supports diverse forms of life and ensures the provision of essential resources for our survival. A clean and sustainable environment is fundamental for clean air, water, and soil, contributing to improved public health and the preservation of biodiversity. In the face of global challenges such as climate change, deforestation, and pollution, it becomes imperative for individuals to be informed and engaged in efforts that promote environmental conservation. The following websites serve as valuable resources, offering insights into the importance of ecological balance and providing practical guidance on how each person can play a role in fostering a healthier and more sustainable world. Explore these platforms to discover information on conservation initiatives, endangered species, and actionable steps for a positive environmental impact.
+			</p>
+			
+			<ul style={{listStyle:'inside'}}>
+				<li><a href='worldwildlife.org'>World Wildlife Fund (WWF)</a></li>
+				<li><a href='https://www.epa.gov/'>Environmental Protection Agency (EPA)</a></li>
+				<li><a href='https://www.nationalgeographic.com/environment/'>WNational Geographic - Environment</a></li>
+				<li><a href='https://www.greenpeace.org/'>Greenpeace</a></li>
+				<li><a href='https://www.nature.org/'>WThe Nature Conservancy</a></li>
+
+			</ul>
 			<NavBar />
 		</div>
 	);
--- my_bank/src/componets/eco_components/Recycle.js
+++ my_bank/src/componets/eco_components/Recycle.js
@@ -1,11 +1,17 @@
 import NavBar from "../NavBar";
 import EcoNavBar from "./EcoNavBar";
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
 
 const Recycle = () => {
 	return (
 		<div>
 			<EcoNavBar />
-			<h1>Recycle</h1>
+			<p  style={{textAlign:'justify', marginLeft:'5vw', marginRight:'5vw' }}>You can recycle materials in exchange for curency. You just have to fill up your (recyclable) bags with paper, plastic, old clothes or broken electronics and come to one of our recycling centers. <a href='https://www.google.com/'>Here</a> is a complete list of centers where you can contribute to a healthier environment!</p>
+			<h1 style={{textAlign:'center'}}>Scan this QR:</h1>
+			<div style={{display:'flex', justifyContent:'center'}}>
+				<QRCode value="userID" style={{alignSelf:'center'}}></QRCode>	
+			</div>
 			<NavBar />
 		</div>
 	);
